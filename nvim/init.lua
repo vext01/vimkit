5e66b57cdb4890ea45ec432234066f4927491760
Edd Barrett <vext01@gmail.com>
2021-10-07 15:25:31 +0100

    Yet more plugins.

--- nvim/init.lua
+++ nvim/init.lua
@@ -36,6 +36,10 @@ require('packer').startup(function()
   use {'phaazon/hop.nvim', as = 'hop'} -- improved navigation
   use 'stevearc/aerial.nvim' -- class/function browser
   use 'folke/trouble.nvim' -- Diagnostic list
+  use 'psliwka/vim-smoothie' -- Smooth scrolling
+  --use 'karb94/neoscroll.nvim' -- Smooth scrolling
+  use 'jbyuki/venn.nvim' -- ASCII art drawings
+  use 'dstein64/nvim-scrollview' -- Display a scrollbar
 
   -- Programming langauges
   use 'rhysd/vim-llvm'
@@ -309,6 +313,47 @@ require("trouble").setup {
   icons = false,
   use_lsp_diagnostic_signs = false
 }
+
+-------
+-- venn
+-------
+
+-- enable or disable keymappings for venn
+function _G.toggle_venn()
+    local venn_enabled = vim.inspect(vim.b.venn_enabled)
+    if(venn_enabled == "nil") then
+        vim.b.venn_enabled = true
+        vim.cmd[[setlocal ve=all]]
+        -- draw a line on HJKL keystokes
+        vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<cr>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<cr>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<cr>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<cr>", {noremap = true})
+        -- draw a box by pressing "f" with visual selection
+        vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<cr>", {noremap = true})
+    else
+        vim.cmd[[setlocal ve=]]
+        vim.cmd[[mapclear <buffer>]]
+        vim.b.venn_enabled = nil
+    end
+end
+-- toggle keymappings for venn using <leader>v
+vim.api.nvim_set_keymap('n', '<leader>v', ":lua toggle_venn()<cr>", { noremap = true})
+
+-------------
+-- scrollview
+-------------
+
+vim.cmd[[hi link ScrollView Search]]
+
+------------
+-- neoscroll
+------------
+
+-- https://github.com/karb94/neoscroll.nvim/issues/23#issuecomment-937840793
+--require('neoscroll').setup({
+--    easing_function = "sine"
+--})
 
 -------------
 -- Misc stuff
